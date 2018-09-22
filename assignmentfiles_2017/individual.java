98c746f414cd5beb270fb8059cb7546fa9d4f925
Unknown <stickfiguredaan@gmail.com>
2018-09-22 16:58:27 +0200

    Add evolving mutation rate, restructure code

--- assignmentfiles_2017/individual.java
+++ assignmentfiles_2017/individual.java
@@ -1,4 +1,33 @@
-public class individual {
-    public double[] genes;
-    public double fitness;
+
+public class Individual {
+	public double[] genes;
+	public double mutationRate;
+	public double fitness;
+
+	public Individual () {
+		this.fitness = -1.0;
+		this.mutationRate = 1.0;
+		this.genes = new double[10];
+		// initialize with random genes
+		for (int i=0; i<genes.length; i++) {
+			double allele = player34.rnd_.nextDouble() * (player34.PROBLEM_RANGE_MAX - player34.PROBLEM_RANGE_MIN);
+    		this.genes[i] = allele;
+		}
+	}
+
+	// copy constructor used for creating offspring
+	public Individual (Individual that) {
+		this.genes = new double[10];
+		this.mutationRate = that.mutationRate;
+		for (int i=0; i<this.genes.length; i++) {
+			this.genes[i] = that.genes[i];
+		}
+		this.fitness = that.fitness;
+	}
+
+	public void evaluate () {
+		if (this.fitness == -1.0) {
+			this.fitness = Math.max(0, (double) player34.evaluation_.evaluate(this.genes));
+		}
+	}
 }
--- assignmentfiles_2017/player34_daan.java
+++ assignmentfiles_2017/player34_daan.java
@@ -5,31 +5,72 @@ import org.vu.contest.ContestEvaluation;
 import java.lang.Math;
 import java.util.Random;
 import java.util.Properties;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 public class player34 implements ContestSubmission
 {
-	private static final int POPULATION_SIZE = 30;
-	private static final int PROBLEM_DIMENSIONALITY = 10;
-	private static final int PROBLEM_RANGE_MIN = -5;
-	private static final int PROBLEM_RANGE_MAX = 5;
-	private static final int NUMBER_OF_PARENTS_EACH_GEN = 5;
+    // fixed parameters
+    public static final int PROBLEM_DIMENSIONALITY = 10;
+    public static final int PROBLEM_RANGE_MIN = -5;
+    public static final int PROBLEM_RANGE_MAX = 5;
 
-	Random rnd_;
-	ContestEvaluation evaluation_;
+    // configurable parameters
+    public static int populationSize_ = 30;
+    public static int parentCountPerGeneration_ = 5;
+
+    // administrative stuff
+    private static final PrintStream oldOut_ = System.out;
+
+    // provided fields (do not touch)
+	public static Random rnd_;
+	public static ContestEvaluation evaluation_;
     private int evaluations_limit_;
 	
 	public player34()
 	{
 		rnd_ = new Random();
+
+        // Disable console output for competition environment
+        // Start with -Dprint flag to enable
+        if (System.getProperty("print") == null) {
+            disableConsolePrinting();
+        }
+
+        // set population size
+        if (System.getProperty("popsize") != null) {
+            populationSize_ = Integer.parseInt(System.getProperty("popsize"));
+        }
+
+        // set how many parents are selected each generation
+        if (System.getProperty("parentcount") != null) {
+            parentCountPerGeneration_ = Integer.parseInt(System.getProperty("parentcount"));
+        }
 	}
-	
+
+    private void programEndOperations () {
+        // enable console printing again for final output of program (doesn't affect online ranking)
+        enableConsolePrinting();
+    }
+
+    private static void disableConsolePrinting () {
+        System.setOut(new PrintStream(new OutputStream() {
+            public void write(int b) {
+                //DO NOTHING
+            }
+        }));
+    }
+
+    private static void enableConsolePrinting () {
+        System.setOut(oldOut_);
+    }
+
 	public void setSeed(long seed)
 	{
 		// Set seed of algortihms random process
@@ -59,80 +100,16 @@ public class player34 implements ContestSubmission
         }
     }
 
-    private List<Individual> initPopulation ()
-    {
-    	List<Individual> population = new ArrayList<Individual>();
-    	for (int i=0; i < POPULATION_SIZE; i++) {
-    		Individual newGuy = new Individual();
-    		newGuy.genes = new double[PROBLEM_DIMENSIONALITY];
-			for (int j=0; j<newGuy.genes.length; j++) {
-    			double allele = rnd_.nextDouble() * (PROBLEM_RANGE_MAX - PROBLEM_RANGE_MIN);
-    			newGuy.genes[j] = allele;
-    		}
-    		population.add(newGuy);
-    	}
-    	return population;
-    }
-
-    private void printPopulation (List<Individual> population) { printPopulation(population, false); }
-    private void printPopulation (List<Individual> population, boolean withGenes)
-    {
-    	System.out.println("Population:");
-    	for (Individual individual : population) {
-    		if (withGenes) {
-    			System.out.printf("Fitness: %g Genes: %s\n", individual.fitness, Arrays.toString(individual.genes));
-    		} else {
-    			System.out.printf("Fitness: %g\n", individual.fitness);
-    		}
-    	}
-    }
-
-    private void evalIndividuals (List<Individual> individuals)
-    {
-    	for (Individual individual : individuals) {
-    		individual.fitness = Math.max(0, (double) evaluation_.evaluate(individual.genes));
-    	}
-    }
-
-    private List<Individual> weightedRandomDraw (List<Individual> population, int drawCount) {
-		List<Individual> populationCopy = new ArrayList<Individual>(population);
-		List<Individual> chosenOnes = new ArrayList<Individual>();
-		
-    	// Weighted random selection from https://stackoverflow.com/questions/6737283/weighted-randomness-in-java
-		for (int i=0; i<drawCount; i++) {
-			// Compute the total weight of all items together
-			double totalWeight = 0.0;
-			for (Individual individual : populationCopy) {
-			    totalWeight += individual.fitness;
-			}
-
-			// Now choose a random item
-			double random = rnd_.nextDouble() * totalWeight;
-			for (Individual individual : populationCopy) {
-			    random -= individual.fitness;
-			    if (random <= 0.0) {
-			    	populationCopy.remove(individual); // prevent replacement 
-			        chosenOnes.add(individual);
-			        break;
-			    }
-			}
-		}
-		return chosenOnes;
-    }
-
     private List<Individual> reproduce (List<Individual> parents)
     {
     	List<Individual> children = new ArrayList<Individual>();
     	for (Individual parent : parents) {
-   			// double median = 0.2;
-			// double lamb = Math.log(2.0)/median;
-			// double u = rnd_.nextDouble();
-			// double mutationFactor = -Math.log(1 - Math.pow(u,2))/lamb; // drawn from exponential distribution
-    		double mutationFactor = Math.pow(rnd_.nextDouble(),3);
-
     		Individual child = new Individual(parent);
+            child.fitness = -1.0; // fitness is not inherited and will need to be re-evaluated
+            double tau = Math.pow(PROBLEM_DIMENSIONALITY, -0.5);
+            child.mutationRate = parent.mutationRate * Math.exp(tau * rnd_.nextGaussian());
 			for (int i=0; i<child.genes.length; i++) {
-				double mutation = rnd_.nextGaussian() * mutationFactor;
+				double mutation = rnd_.nextGaussian() * child.mutationRate;
 				if (Math.abs(child.genes[i] + mutation) >= 5.0) {
 					mutation *= -1.0;
 				}
@@ -145,56 +122,34 @@ public class player34 implements ContestSubmission
     
 	public void run()
 	{
-        // System.out.println("Ohi");
-        // ClassLoader cl = ClassLoader.getSystemClassLoader();
-        // URL[] urls = ((URLClassLoader)cl).getURLs();
-        // for(URL url: urls){
-        // 	System.out.println(url.getFile());
-        // }
-        // System.out.println("uhuh");
-        // System.out.println(System.getProperty("java.classpath"));
-        // System.out.println("hmmm");
-        // Individual bla = new Individual();
+        // Initialize population
+        Population population = new Population(populationSize_);
+        population.evaluate();
+        population.print();
 
-		// testing
-		// int individualCounter = 0;
-  //       List<Individual> population = initPopulation();
-		// for (Individual individual : population) {
-		// 	individual.fitness = individualCounter++;
-		// }
-		// printPopulation(population, true);
-		// population = weightedRandomDraw(population, POPULATION_SIZE);
-		// printPopulation(population, true);
-		// if (true) {
-		// 	return;
-		// }
-
-		// Run your algorithm here
-        // init population
-        List<Individual> population = initPopulation();
-        // calculate fitness
-        int evaluationCount = 0;
-        evalIndividuals(population);
-		evaluationCount += POPULATION_SIZE;
-        printPopulation(population);
-        while (evaluationCount+NUMBER_OF_PARENTS_EACH_GEN < evaluations_limit_) {
+        int evaluationCount = populationSize_;
+        while (evaluationCount+parentCountPerGeneration_ < evaluations_limit_) {
             // Select parents
-        	List<Individual> parents = weightedRandomDraw(population, NUMBER_OF_PARENTS_EACH_GEN);
+        	List<Individual> parents = population.weightedRandomDraw(parentCountPerGeneration_);
             // Apply crossover / mutation operators
             List<Individual> children = reproduce(parents);
-            // Check fitness of unknown fuction
-	        evalIndividuals(children);
             population.addAll(children);
-            evaluationCount += NUMBER_OF_PARENTS_EACH_GEN; // same as number of children atm
+            // Check fitness of unknown fuction
+            population.evaluate(); // skips those who already have been evaluated
+            evaluationCount += parentCountPerGeneration_; // same as number of children atm
             // Select survivors
-            population = weightedRandomDraw(population, POPULATION_SIZE);
+            population.individuals = population.weightedRandomDraw(populationSize_);
 
-	        if (evaluationCount % (evaluations_limit_/100) == 0) {
+            // optionally print statistics sometimes
+	        if (evaluationCount % (evaluations_limit_/10) == 0) {
 	        	System.out.printf("Evaluation count: %d / %d\n", evaluationCount, evaluations_limit_);
-	        	printPopulation(population);
+	        	population.print();
 	        }
         }
         System.out.println("We're done here");
-        printPopulation(population, true);
+        population.print(true);
+
+        // java has no destructor, so explicitly call this here
+        programEndOperations();
 	}
 }
