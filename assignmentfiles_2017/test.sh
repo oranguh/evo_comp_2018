1ac96028e2b24173a0aa6505f26e66b586607019
Daan <stickfiguredaan@gmail.com>
2018-10-06 19:14:31 +0200

    Run tests with population size 100

--- assignmentfiles_2017/test.sh
+++ assignmentfiles_2017/test.sh
@@ -45,7 +45,7 @@ do
 	for (( i=1; i<=runCount; i++ ))
 	do
 		((seed++))
-		java -Dcsv -jar testrun.jar -submission=player34 -evaluation="${evalName}" -seed="${seed}" > "tests/${testName}/${evalName}/metrics_${seed}.csv"
+		java -Dpopsize=100 -Dcsv -jar testrun.jar -submission=player34 -evaluation="${evalName}" -seed="${seed}" > "tests/${testName}/${evalName}/metrics_${seed}.csv"
 	done
 	wait
 	# Run python script that loads csv files for each evaluation and plots metrics
