e51d501ae7eb0bdd2b3a88ed474c7b56b5fecdc0
Daan <stickfiguredaan@gmail.com>
2018-10-06 19:13:42 +0200

    Add MBF and convergence metrics to plots, improve visuals

--- assignmentfiles_2017/plot.py
+++ assignmentfiles_2017/plot.py
@@ -27,13 +27,33 @@ for csvFileName in csvFileNames:
 	with open(csvFileName, newline='') as csvFile:
 		fileContents.append(np.loadtxt(csvFile, delimiter=";", skiprows=1))
 
-# Put all csv contents into matrix
+# Put all csv contents into matrix (dims: Steps - Metrics - Runs)
 metrics = np.dstack(fileContents)
 metricCount = metrics.shape[1]
 runCount = metrics.shape[2]
 
 # Calculate means of each metric
 means = metrics.mean(axis=2)
+
+# Calculate MBF
+bestFitnesses = metrics[:,1,:].max(axis=0)
+meanBestFitness = bestFitnesses.mean()
+stdBestFitness = bestFitnesses.std()
+
+# Find steps at which some percentage of best fitness was reached (our convergence metric)
+convergenceRatio = 0.90
+def getConvergenceStep(run):
+	bestFitness = run[:,1].max(axis=0)
+	convergenceValue = bestFitness * convergenceRatio
+	convergenceStep = -1
+	for measurement in run:
+		if measurement[1] >= convergenceValue:
+			convergenceStep = measurement[0]
+			break
+	return convergenceStep
+convergenceSteps = np.array([getConvergenceStep(run) for run in np.rollaxis(metrics, 2, 0)])
+meanConvergence = convergenceSteps.mean()
+stdConvergence = convergenceSteps.std()
 
 # Onto plotting!
 colors = ['black', 'tab:red', 'tab:blue', 'tab:green', 'gray'] # more than 4 metrics? fuck you
@@ -57,21 +77,24 @@ for i in range(2, metricCount):
 	axis.spines["right"].set_position(('outward', 50*(i-2)))
 	axes.append(axis)
 
-# Plot vague line for each run first
-alpha = min(2.0 / runCount, 1.0)
-for i in range(1, 2):
-	axis = axes[i-1]
-	axis.plot(means[:,0], metrics[:,i,:], color=colors[i], alpha=alpha)
+# Plot vague fitness curve for each run
+alpha = 0.2
+fitnessAxis = axes[0]
+fitnessAxis.plot(means[:,0], metrics[:,1,:], color=colors[1], alpha=alpha)
 
 # Plot averages as thicker lines
-outline = mpe.withStroke(linewidth=1.0, foreground='black')
-for i in reversed(range(1, metricCount)):
+outline = mpe.withStroke(linewidth=2, foreground='black')
+for i in range(1, metricCount):
 	axis = axes[i-1]
 	axis.plot(means[:,0], means[:,i], color=colors[i], alpha=1.0, label=metricNames[i], path_effects=[outline])
+
+# Plot MBF and convergence time as cross, where crossover is average, and lengths indicate std
+fitnessAxis.plot([meanConvergence, meanConvergence], [meanBestFitness - stdBestFitness, meanBestFitness + stdBestFitness], 'r-')
+fitnessAxis.plot([meanConvergence - stdConvergence, meanConvergence + stdConvergence], [meanBestFitness, meanBestFitness], 'r-')
 
 # Some further layout
 plt.title(args.title)
 fig.tight_layout()
 
 # Save to file
-plt.savefig(args.dst, bbox_inches='tight')
+plt.savefig(args.dst, bbox_inches='tight', dpi=150)
