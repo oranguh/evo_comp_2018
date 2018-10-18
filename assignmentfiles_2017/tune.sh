a90eef1d3ab69db408886c3d3ee5f4cf1c02d1bf
ximua00 <kishanarendra@gmail.com>
2018-10-18 23:47:25 +0200

    Fixed tuning bug and decreaseg hyperparameters search

--- assignmentfiles_2017/tune.sh
+++ assignmentfiles_2017/tune.sh
@@ -1,15 +1,15 @@
 # evalNames=( "BentCigarFunction" "SchaffersEvaluation" )
 evalNames=( "SchaffersEvaluation" )
 
-sigmas=(0.1 0.3 0.4 0.5 0.6 0.8 1)
-islandNs=(1 2 4 5 10)
-epochs=(10 20 30 40 50)
-popsizes=(50 100 200 400 500)
-parentcounts=(2 5 10 15 20 30)
-migrationsizes=(1 2 5 10 15 20)
+sigmas=(0.2 0.3 0.5 0.8)
+islandNs=(1 4 10)
+epochs=(10 20 40)
+popsizes=(50 100 200)
+parentcounts=(5 10 20)
+migrationsizes=(2 5 10)
 aritys=(2 5)
 recombinationProbabilitys=(0.5 0.7)
-ks=(2 5 10)
+ks=(2 10)
 
 defaultParameters="-Dparentselection=tournament -Dsurvivorselection=tournament -Drecombination=m-1crossover -Dmutation=adaptivegauss"
 
@@ -31,14 +31,6 @@ defaultParameters="-Dparentselection=tournament -Dsurvivorselection=tournament -
 # 	fi
 # done
 
-# Fitness sharing on
-#TODO: Add
-#-Dmigrationsize=2
-#-Dpopsize=100
-#-Dparentcount=10
-# -Darity
-# -DrecombinationProbability
-# -Dk
 
 fitnessBool=true
 for islandN in "${islandNs[@]}"; do
@@ -71,7 +63,7 @@ for islandN in "${islandNs[@]}"; do
 						for recombinationProbability in "${recombinationProbabilitys[@]}"; do
 							for k in "${ks[@]}"; do
 								runName="$islandN;0;true;${sigma};$popsize;$parentcount;$arity;$recombinationProbability;$k"
-								parameterString="-Dislands=${islandN} -Depochsize=${epoch} -Dsharefitness -Dsigma=${sigma} -Dpopsize=${popsize} -Dparentcount=${parentcount} -Darity=${arity} -DrecombinationProbability=${recombinationProbability} -Dk=${k}"
+								parameterString="-Dislands=${islandN} -Dsharefitness -Dsigma=${sigma} -Dpopsize=${popsize} -Dparentcount=${parentcount} -Darity=${arity} -DrecombinationProbability=${recombinationProbability} -Dk=${k}"
 								parameterString="${parameterString} $defaultParameters"
 								./test.sh $runName 10 $parameterString
 							done
